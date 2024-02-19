380c932f96705198b35ce460b1fe4eb96f50375a
jmis <324872+jmis@users.noreply.github.com>
2024-02-18 21:41:25 -0500

    Remove RemoteXY, add BT serial

--- src/Constants.h
+++ src/Constants.h
@@ -1,8 +1,4 @@
-#define REMOTEXY_MODE__ESP32CORE_BLE
-#define REMOTEXY_BLUETOOTH_NAME "PM-25MV Z LIFT"
-
 #include <Arduino.h>
-#include <RemoteXY.h>
 
 #define MICROSTEPS 32
 #define STEPPER_STEPS_PER_ROTATION 200
@@ -18,19 +14,3 @@
 #define FEED_RATE_PIN 36
 #define ROTARY_PIN_A 18
 #define ROTARY_PIN_B 19
-
-#pragma pack(push, 1)
-uint8_t RemoteXY_CONF[] = { 255,8,0,1,0,78,0,16,8,1,66,1,27,43,9,37,2,26,129,0,
-  2,13,42,4,17,77,83,116,101,112,115,32,32,32,32,32,32,32,77,97,
-  120,32,70,32,32,32,32,32,32,32,32,32,70,105,110,101,0,7,52,42,
-  19,18,5,2,26,2,7,44,22,19,17,5,2,26,2,1,7,52,2,19,
-  18,5,2,26,2 };
-struct
-{
-	int16_t microsteps;
-	float max_rpm;
-	int16_t fine_step;
-	int8_t percent_max_rpm;
-	uint8_t connect_flag;
-} RemoteXY;
-#pragma pack(pop)
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 #include "Constants.h"
 #include <AccelStepper.h>
 #include <AceButton.h>
-#include <nvs_flash.h>
-#include <EEPROM.h>
 #include <RotaryEncoder.h>
 #include <FastAccelStepper.h>
 #include "LowPassFilter.h"
+#include "nvs_flash.h"
+#include "BluetoothSerial.h"
 
 using namespace ace_button;
 
+BluetoothSerial SerialBT;
 RotaryEncoder fineStepEncoder(ROTARY_PIN_A, ROTARY_PIN_B, RotaryEncoder::LatchMode::TWO03);
 FastAccelStepperEngine engine = FastAccelStepperEngine();
-FastAccelStepper* stepper;
+FastAccelStepper *stepper;
 ButtonConfig cw_button_config;
 AceButton cw_button(&cw_button_config);
 ButtonConfig ccw_button_config;
@@ -23,7 +24,7 @@ void processPotentiometer()
 	static auto lastSteps = 0;
 
 	auto potentiometerReading = analogRead(FEED_RATE_PIN);
-	auto readPercent = (int) map(potentiometerReading, 0, 4095, 0, 100);
+	auto readPercent = (int)map(potentiometerReading, 0, 4095, 0, 100);
 	float filteredPercent = (int)potentiometerFilter(readPercent);
 	auto currentSteps = MAX_US_PER_STEP / (filteredPercent / 100);
 
@@ -32,17 +33,17 @@ void processPotentiometer()
 		lastSteps = currentSteps;
 		stepper->setSpeedInUs(currentSteps);
 		stepper->applySpeedAcceleration();
-		RemoteXY.percent_max_rpm = filteredPercent;
 	}
 }
 
 void processEncoder()
 {
 	static auto lastPosition = 0;
+
 	auto currentPosition = fineStepEncoder.getPosition();
 	float delta = currentPosition - lastPosition;
 	if (delta == 0) return;
-	stepper->move((long) (delta / 48 * 6400L));
+	stepper->move((long)(delta / 48 * 6400L));
 	lastPosition = currentPosition;
 }
 
@@ -50,7 +51,6 @@ void inputProcessingTask(void *pvParameter)
 {
 	while (1)
 	{
-		RemoteXY_Handler();
 		cw_button.check();
 		ccw_button.check();
 		processPotentiometer();
@@ -61,19 +61,17 @@ void inputProcessingTask(void *pvParameter)
 
 extern "C" void app_main()
 {
+	ESP_ERROR_CHECK(nvs_flash_init());
 	Serial.begin(115200);
-	nvs_flash_init();
+	SerialBT.begin("HelloWorld");
+	SerialBT.setPin("1234");
 
-	RemoteXY_Init();
-	RemoteXY.connect_flag = 1;
-	RemoteXY.max_rpm = 7;
-
-	attachInterrupt(digitalPinToInterrupt(ROTARY_PIN_A),[]() { fineStepEncoder.tick(); }, CHANGE);
-	attachInterrupt(digitalPinToInterrupt(ROTARY_PIN_B),[]() { fineStepEncoder.tick(); }, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(ROTARY_PIN_A), []() { fineStepEncoder.tick(); }, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(ROTARY_PIN_B), []() { fineStepEncoder.tick(); }, CHANGE);
 
 	pinMode(CW_PIN, INPUT_PULLUP);
 	cw_button.init(CW_PIN);
-	cw_button.setEventHandler([](AceButton* b, uint8_t eventType, uint8_t state)
+	cw_button.setEventHandler([](AceButton *b, uint8_t eventType, uint8_t state)
 	{
 		if (eventType == AceButton::kEventPressed) stepper->runForward();
 		else if (eventType == AceButton::kEventReleased) stepper->forceStop();
@@ -81,7 +79,7 @@ extern "C" void app_main()
 
 	pinMode(CCW_PIN, INPUT_PULLUP);
 	ccw_button.init(CCW_PIN);
-	ccw_button.setEventHandler([](AceButton* b, uint8_t eventType, uint8_t state)
+	ccw_button.setEventHandler([](AceButton *b, uint8_t eventType, uint8_t state)
 	{
 		if (eventType == AceButton::kEventPressed) stepper->runBackward();
 		else if (eventType == AceButton::kEventReleased) stepper->forceStop();
@@ -99,6 +97,3 @@ extern "C" void app_main()
 }
 
 // esp_log_level_set("*", ESP_LOG_NONE);
-// EEPROM.begin(1024);
-// EEPROM.writeUInt(6400);
-// EEPROM.commit();
